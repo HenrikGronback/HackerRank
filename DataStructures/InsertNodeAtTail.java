bab2e1060b30ecb41f60a17ab576d6ee76fab04f
Henrik Grönbäck <henrik.gronback@hotmail.com>
2020-07-01 00:33:21 +0200

    Add files via upload

---
+++ DataStructures/InsertNodeAtTail.java
@@ -0,0 +1,106 @@
+
+    import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+    public class InsertNodeAtTail {
+
+        static class SinglyLinkedListNode {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData) {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        static class SinglyLinkedList {
+            public SinglyLinkedListNode head;
+
+            public SinglyLinkedList() {
+                this.head = null;
+            }
+
+        }
+
+        public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) throws IOException {
+            while (node != null) {
+                System.out.println((String.valueOf(node.data)));
+
+                node = node.next;
+
+                if (node != null) {
+                    System.out.println((sep));
+                }
+            }
+        }
+
+        // Complete the insertNodeAtTail function below.
+
+        /*
+         * For your reference:
+         *
+         * SinglyLinkedListNode {
+         *     int data;
+         *     SinglyLinkedListNode next;
+         * }
+         *
+         */
+        static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+            SinglyLinkedListNode new_node = new SinglyLinkedListNode(data);
+            if(head == null)
+            {
+                head = new_node;
+                return head;
+
+            }
+
+            SinglyLinkedListNode  current_node = head;
+            while(current_node.next != null){
+                current_node = current_node.next;
+            }
+            current_node.next = new_node;
+            //System.out.println("currentNext: "+current_node.next.data);
+            //System.out.println("headNext: "+head.next.data);
+            return head;
+
+
+        }
+
+        private static final Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) throws IOException {
+            //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+                llist.head = llist_head;
+
+            }
+
+
+
+            printSinglyLinkedList(llist.head, "\n");
+            //bufferedWriter.newLine();
+
+           // bufferedWriter.close();
+
+            scanner.close();
+        }
+    }
+
+
